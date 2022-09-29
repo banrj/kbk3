8e6c9d7ff85b12552035eb29e87020cf77601fdf
banrj <eh.ehr@bk.ru>
2022-09-29 14:45:23 +0200

    logic operation

---
+++ kbk_lesson4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Богомолв Даниил 13 вариант базовый
+try
+{
+    //Console.WriteLine("Введите число A: ");
+    int FirstNumber = 46;
+    //int.Parse(Console.ReadLine());
+    //Console.WriteLine("Введите число B: ");
+    int SecondNumber = 60;
+    //int.Parse(Console.ReadLine());
+    //Console.WriteLine("Введите число C: ");
+    int ThirdNumber = 390;
+    //int.Parse(Console.ReadLine());
+
+    if ((FirstNumber < 45 && SecondNumber > 45 && ThirdNumber > 45) ||
+        (FirstNumber > 45 && SecondNumber > 45 && ThirdNumber < 45) ||
+        (FirstNumber > 45 && SecondNumber < 45 && ThirdNumber > 45))
+    {
+        Console.WriteLine(true);
+    }
+    else Console.WriteLine(false);
+}
+
+catch
+{
+
+}
